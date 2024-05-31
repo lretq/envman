c867f20983d9064271175222ea1f0cbee9aedbea
Vincent Schweiger <vincent.schweiger@xolley.de>
2024-06-01 01:34:49 +0200

    implement idea with fish function

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 .PHONY: install
 install:
 	install -Dm 0755 envman $(HOME)/.local/bin/envman
+	install -D extras/envman.fish $(HOME)/.config/fish/functions/envman.fish
 	install completions/envman.fish $(HOME)/.config/fish/completions/envman.fish
---
+++ extras/envman.fish
@@ -0,0 +1,15 @@
+function envman 
+    set envdir "$HOME/.envman/"
+    cp "$envdir/active" "$envdir/.tmp"
+    command envman $argv
+    set ret "$status"
+    if ! test "$ret" -eq 0 
+        return $ret
+    end
+    if test $argv[1] = "activate"; or test $argv[1] = "edit"
+        for var in $(cat "$envdir/.tmp"|grep export|awk '{sub(/=.*/,""); print $2}')
+            set --erase "$var"
+        end 
+        source "$envdir/active"
+    end
+end
